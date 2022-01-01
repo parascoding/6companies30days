176786eb6f8d58582767bbcbf4795031e7698d9f
Paras Yadav <62833829+parascoding@users.noreply.github.com>
2022-01-01 12:14:46 +0530

    Create Q6 gcdOfStrings.java

---
+++ Goldman Sachs/Q6 gcdOfStrings.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)) return "";
+        int gc=gcd(str1.length(),str2.length());
+        return str1.substring(0,gc);
+    }
+    private int gcd(int a,int b){
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+}
